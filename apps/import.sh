86b3cef8ee32cffd2487264255c5b72922b45a45
Deusmar Junior <deusmar.junior@hotmail.com>
2018-10-29 09:33:21 -0300

    Alteração na localização dos arquivos.

--- apps/import.sh
+++ apps/import.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 . apps/vhautlinux.sh
 . apps/dictionary.sh
+
+. apps/chrome.sh
+. apps/sublime.sh
+. apps/vscode.sh
+. apps/editors.sh
--- tools/import.sh
+++ tools/import.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-. tools/chrome.sh
 . tools/java.sh
 . tools/npm.sh
-. tools/sublime.sh
-. tools/vscode.sh
-. tools/editors.sh
